44fce50838c29e344ee569630f5e26931a7637e6
kathy chan <katriana73@yahoo.com>
2020-01-25 02:16:32 -0500

    added input file and more to heapify

--- heap.py
+++ heap.py
@@ -1,7 +1,12 @@
-arr = [None,9,7,6,12,8,17] # unsorted
+myFile = open('test.in', 'r')
+arr =  [int(i) for i in myFile.readline().strip().split()] # unsorted
+arr.insert(0, None)
+# print(arr)
 midpoint = 0
 
-def heapify(arr):
+def heapify(arr, elem):
+
+    isLeaf = False
 
     if len(arr) % 2 == 0: # even
         midpoint = len(arr)/2
@@ -11,15 +16,16 @@ def heapify(arr):
     rightchild = midpoint*2 + 1
     leftchild = midpoint*2
     
+    # default is root
     myMax = arr[midpoint]
     bigger = midpoint
 
     if rightchild is not None:
-        if arr[rightchild] > arr[leftchild]:
+        if arr[rightchild] > arr[leftchild]: # if right child is bigger
             myMax = arr[rightchild]
             bigger = rightchild
-        elif arr[rightchild] < arr[leftchild]:
+        elif arr[rightchild] < arr[leftchild]: # if left child is bigger
             myMax = arr[leftchild]
             bigger = leftchild
 
-    arr[midpoint], arr[bigger] = arr[bigger], arr[midpoint]
+    arr[midpoint], arr[bigger] = arr[bigger], arr[midpoint] # swap
