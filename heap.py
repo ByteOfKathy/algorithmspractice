17f9ae991b61fd7dd43b3ff4576995cc44538f35
kathy chan <katriana73@yahoo.com>
2020-01-25 01:45:42 -0500

    added to heapify method

--- heap.py
+++ heap.py
@@ -8,3 +8,18 @@ def heapify(arr):
     else: # odd
         midpoint = len(arr)/2 + 1
 
+    rightchild = midpoint*2 + 1
+    leftchild = midpoint*2
+    
+    myMax = arr[midpoint]
+    bigger = midpoint
+
+    if rightchild is not None:
+        if arr[rightchild] > arr[leftchild]:
+            myMax = arr[rightchild]
+            bigger = rightchild
+        elif arr[rightchild] < arr[leftchild]:
+            myMax = arr[leftchild]
+            bigger = leftchild
+
+    arr[midpoint], arr[bigger] = arr[bigger], arr[midpoint]
